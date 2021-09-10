880a2265f6864d71e59c477a3036743a04910891
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-09-10 13:29:37 +0530

    dispatching actions and subscribe store

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import store from "./store";
-console.log(store);
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store Changed", store.getState());
+  // This is where we do UI changes when state gets updated like if you are using Vanilla JS so we will do DOM stuff here or if you are using react so you will re-render your UI here.
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
