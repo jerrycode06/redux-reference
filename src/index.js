19bc3a6d354d818fd2f4594e4222ce85263e1a90
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-09-10 13:49:56 +0530

    action creator

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store Changed", store.getState());
   // This is where we do UI changes when state gets updated like if you are using Vanilla JS so we will do DOM stuff here or if you are using react so you will re-render your UI here.
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
 
 unsubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
