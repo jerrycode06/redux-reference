e477b143793bb013582547375de0267c319ae66e
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-09-10 17:30:59 +0530

    Redux from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actionCreator";
+import store from "./customStore";
+import { bugAdded, bugRemoved } from "./actionCreator";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store Changed", store.getState());
-  // This is where we do UI changes when state gets updated like if you are using Vanilla JS so we will do DOM stuff here or if you are using react so you will re-render your UI here.
+store.subscribe(() => {
+  console.log("Store Changed");
 });
 
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugAdded("Bug 2"));
-
-store.dispatch(bugResolved(1));
-
-unsubscribe();
-store.dispatch(bugRemoved(1));
-
+store.disptach(bugAdded("Bug 1"));
+store.disptach(bugAdded("Bug 2"));
+store.disptach(bugRemoved(1));
 console.log(store.getState());
+
+// import store from "./store";
+// import { bugAdded, bugRemoved, bugResolved } from "./actionCreator";
+
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store Changed", store.getState());
+//   // This is where we do UI changes when state gets updated like if you are using Vanilla JS so we will do DOM stuff here or if you are using react so you will re-render your UI here.
+// });
+
+// store.dispatch(bugAdded("Bug 1"));
+// store.dispatch(bugAdded("Bug 2"));
+
+// store.dispatch(bugResolved(1));
+
+// unsubscribe();
+// store.dispatch(bugRemoved(1));
+
+// console.log(store.getState());
