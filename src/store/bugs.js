ec6bcfae2f135d166c52d71e12024f37b0f0dcf8
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-10-08 01:03:59 +0530

    code cleanup with redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,32 +1,71 @@
-import { createAction } from "@reduxjs/toolkit";
-
-// Actions Creaters
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
-
-// Reducers
+// import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case bugResolved.type:
-      return state.map((bug) =>
-        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-      );
-    default:
-      return state;
-  }
-}
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // action ==> Action Handlers
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id !== action.payload.id);
+      bugs[index].resolved = true;
+    },
+  },
+});
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
+
+// Actions Creaters
+// export const bugAdded = createAction("bugAdded");
+// export const bugRemoved = createAction("bugRemoved");
+// export const bugResolved = createAction("bugResolved");
+
+// Reducers
+
+// export default createReducer([], {
+//   //actions : functions (event => event handler)
+//   [bugAdded.type]: (bugs, action) => {
+//     bugs.push({
+//       id: ++lastId,
+//       description: action.payload.description,
+//       resolved: false,
+//     });
+//   },
+//   [bugResolved.type]: (bugs, action) => {
+//     const index = bugs.findIndex((bug) => bug.id !== action.payload.id);
+//     bugs[index].resolved = true;
+//   },
+//   bugRemoved: (bugs, action) => {},
+// });
+
+// export default function reducer(state = [], action) {
+//   switch (action.type) {
+//     case bugAdded.type:
+//       return [
+//         ...state,
+//         {
+//           id: ++lastId,
+//           description: action.payload.description,
+//           resolved: false,
+//         },
+//       ];
+//     case bugRemoved.type:
+//       return state.filter((bug) => bug.id !== action.payload.id);
+//     case bugResolved.type:
+//       return state.map((bug) =>
+//         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+//       );
+//     default:
+//       return state;
+//   }
+// }
