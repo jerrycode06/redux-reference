120dfdd9f23d0252ce7eef74ebfb498c04651855
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-10-17 00:57:14 +0530

    writing unit test for math module

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,13 @@
-it("first test", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("should return true if given even number", () => {
+    const result = isEven(4);
+    expect(result).toEqual(true);
+  });
+
+  it("should return false if given odd number", () => {
+    const result = isEven(9);
+    expect(result).toEqual(false);
+  });
+});
