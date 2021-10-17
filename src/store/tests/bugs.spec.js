39f9e9d1c698c3b2cdbf62da2c391c096611fb51
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-10-17 14:14:47 +0530

    Mocking HTTP calls with axios adapter

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,15 +1,34 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { addBug } from "../bugs";
 import configureStore from "../configureStore";
 
 //SOCIAL TEST - ONLY CARES ABOUT BEHAVIOUR, that means if we change implementation in future, behaviour will be the same
+/*Here we will make fake API call with axios-mock-adapter to perform actual unit test*/
 describe("bugsSlice", () => {
   it("should handle the addBug action", async () => {
-    const store = configureStore();
     const bug = { description: "aa" };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
+
+/* This test making connection with external bakcend which doesn't lie in the category of Unit Test, If some time 
+    there is problem in backend, this test will fail */
+// describe("bugsSlice", () => {
+//   it("should handle the addBug action", async () => {
+//     const store = configureStore();
+//     const bug = { description: "aa" };
+//     await store.dispatch(addBug(bug));
+//     expect(store.getState().entities.bugs.list).toHaveLength(1);
+//   });
+// });
 
 // SOLITARY TEST - SO MUCH IMPLEMENTATION DETAILS HERE
 // describe("bugsSlice", () => {
