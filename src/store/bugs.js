3ef31ef387fb06a13a0e4c191f98ce51f6b6754f
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-10-18 12:46:09 +0530

    test coverage and getUnresolvedBugs Test

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -111,13 +111,13 @@ export const assignBugToUser = (bugId, userId) =>
 
 export const getUnresolvedBugs = createSelector(
   (state) => state.entities.bugs,
-  (bugs) => bugs.filter((bug) => !bug.resolved)
+  (bugs) => bugs.list.filter((bug) => !bug.resolved)
 );
 
 export const getBugsByUser = (userId) =>
   createSelector(
     (state) => state.entities.bugs,
-    (bugs) => bugs.filter((bug) => bug.userId === userId)
+    (bugs) => bugs.list.filter((bug) => bug.userId === userId)
   );
 
 // Actions Creaters
